96008935aa2c4eb772c147dcf4eaa51587813b0d
William Kirrage <2354535k@student.gla.ac.uk>
2018-01-13 11:55:45 +0000

    TWJ 4.1 created template and defined template variable in view

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <br/> Link to about page: <a href='/rango/about/'>About</a> ")
+    #some kind of dictionary that defines template things - template variable called boldmessage
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    #returns response to client, render function takes user input, template
+    #filename amd context dictionary - mash with template to form full html page...
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     return HttpResponse("Rango says here is the about page. <br/> Link back to home page: <a href='/rango/'>Index</a> ")

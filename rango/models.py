e2b82a64f9e59b0a142d90748c7593bcf1c87a57
William Kirrage <2354535k@student.gla.ac.uk>
2018-01-14 16:39:19 +0000

    TWJ5.3

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. edited 5.3 creating models
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True) #every category must be unique
+
+    def __str__(self):
+        return self.name
+
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0) #page will start with 0 views
+
+    def __str__(self):
+        return self.title
+
+    

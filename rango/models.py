04ee8b767264e82efbb62391e496e0022530f64a
William Kirrage <2354535k@student.gla.ac.uk>
2018-01-19 18:39:42 +0000

    working on 6.3

--- rango/models.py
+++ rango/models.py
@@ -1,13 +1,20 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here. edited 5.3 creating models
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True) #every category must be unique
     views = models.IntegerField(default=0) #chapter 5 exercise - add views
     likes = models.IntegerField(default=0) #add likes. both with default 0
+    slug = models.SlugField(unique=True) #chapter 6.3 making good urls
+
+    #override save method
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
     
     class Meta:
-        verbose_name_plural = 'Categories'
+        verbose_name_plural = 'categories' #capital letter on categories??
 
     def __str__(self):
         return self.name
--- rango/views.py
+++ rango/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from rango.models import Category
 
 def index(request):
     #some kind of dictionary that defines template things - template variable called boldmessage
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    #chapter 6 - query db for list of all categories, order by number of likes (desc)
+    #then retreive top 5 only. place list in context_dict dictionary (which is passed
+    #to template engine
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+    
     #returns response to client, render function takes user input, template
     #filename amd context dictionary - mash with template to form full html page...
-    return render(request, 'rango/index.html', context=context_dict)
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
     context_dict = {'boldmessage2': "I do hope this works"}

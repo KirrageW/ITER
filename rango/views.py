56753dda3ce98a5124282f99f7d41bcb25d6df0c
William Kirrage <2354535k@student.gla.ac.uk>
2018-01-19 19:13:53 +0000

    6.3 still, added template for category slugs pages

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from rango.models import Category
+from rango.models import Page
 
 def index(request):
     #some kind of dictionary that defines template things - template variable called boldmessage
@@ -18,6 +19,46 @@ def index(request):
 def about(request):
     context_dict = {'boldmessage2': "I do hope this works"}
     return render(request, 'rango/about.html', context=context_dict)
+
+#6.3 making category pages accessible via slugs 
+def show_category(request, category_name_url):
+    #create context dictionary as per - gets passed to template renderer eng.
+    context_dict = {}
+
+    try:
+        #find category slu with given name
+        #if no - get() method raises exception
+        #if yes - get() method returns one model
+        category = Category.objects.get(slug=category_name_slug)
+
+        #retrieve all associated pages
+        #filter() returns list pf page objects or raises exception
+        pages = Page.objects.filter(category=category)
+
+        #adds our results list to template context under name pages
+        context_dict['pages'] = pages
+        #we also add category object from
+        #database to context dictionary
+        #used in template to verify category
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        #catches exceptions - no category found
+        #template display no category message
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context_dict)
+
+#basically - define context dictionary,
+#           - extract data from models, or catch exceptions
+#           - add relevant data to context dicitonary
+#           - which category, determined by using value passed
+#               in parameter category_name_slug to the show_category() view
+#               function
+#           - if this category slug is found in category model, we pull associated pages, and
+#               add to context_dict
+    
+    
 
 
 
